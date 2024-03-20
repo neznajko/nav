90bbb55e041d59269ed91858cbf403fc4d31f0fd
we4er <neznajko@yahoo.com>
2024-03-20 04:20:45 +0200

    folder

--- folder.js
+++ folder.js
@@ -4,13 +4,26 @@
 ////////////////////////////////////////////////////////////////
 console.log( 'floating......' );
 ////////////////////////////////////////////////////////////////
-const check = document.querySelector( ".folder" )
-                      .querySelector( ".check" );
-const out = document.querySelector( ".folder" )
-                    .querySelector( ".out" );
-out.addEventListener( "click", e => {
-    // here change the z-index of the out folder base
-});
+const folder = document.querySelector( ".folder" );
+const check = folder.querySelector( ".check" );
+const _this = folder.querySelector( ".this"  );
+const out   = folder.querySelector( ".out"   );
+const check_base = folder.querySelector( ".check-base" );
+const _this_base = folder.querySelector( ".this-base"  );
+const out_base   = folder.querySelector( ".out-base"   );
+const brand = folder.querySelector( ".folder-brand" );
+let zindex = 3;
+
+function addEventListener( link, base ){
+    link.addEventListener( "click", e => {
+        link.style.zIndex = zindex;
+        base.style.zIndex = zindex;
+        brand.style.zIndex = ++zindex;
+    });
+}
+addEventListener( check, check_base );
+addEventListener( _this, _this_base );
+addEventListener( out, out_base );
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
