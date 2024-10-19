d594e47f2cf454dd471242243c48cddc15e36b86
neznajko <neznajko@yahoo.com>
2024-10-20 01:15:28 +0300

    dropdown

--- dropdown.js
+++ dropdown.js
@@ -1,8 +1,9 @@
 ////////////////////////////////////////////////////////////////
-console.log( "dropdown.." );
+console.log( "dropdown .." );
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 const anime = document.querySelector( '.dropdown_anime' );
+const link = document.querySelector( '.dropdown_link' );
 const icon = anime.querySelector( 'i' );
 const menu = document.querySelector( '.dropdown_menu' );
 const plus = document.querySelector( '.dropdown_plus' );
@@ -13,7 +14,7 @@ const spam = plus.querySelector( 'span' );
 let key = false;
 ////////////////////////////////////////////////////////////////
 // assing this to .dropdown_anime
-anime.addEventListener( 'click', e => {
+link.addEventListener( 'click', e => {
     if( key ){
         icon.classList.add( 'ccw' );
         icon.classList.remove( 'cw' ); 
@@ -54,4 +55,26 @@ icon.addEventListener( 'animationstart', e => {
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
-
+// 1.4.17. A rain is falling, vertically. The speed of raindrops
+// is u. On the [asphalt] with velocity v, is rolling a football
+// ball. How many times for a given [ period ] of time number of
+// raindrops </fallen> on the ball will increase with respect of
+// non moving ball. Is </the> answer change [if] the ball is not
+// spherical? 
+////////////////////////////////////////////////////////////////
+// I'm very proud with my translation, of course I have to thank
+// Google Translate!
+////////////////////////////////////////////////////////////////
+// Ok so if we have a surface ℥ for period of time ∆t the 
+// raindrops with velocity u will take a volume u∆t℥, so the
+// number of raindrops per unit time ∆N|∆t will be proportional
+// to u℥, in system moving with the ball the speed of the
+// raindrops is √(v² + u²), and cos the ball is symmetrical the
+// surface is the same, so the ratio will be √(v² + u²)|u =
+// = √(1 + v²|u²). If the ball is not symmetrical than the drops
+// falling at different angles will see different surfaces, so
+// the answer will be different
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
