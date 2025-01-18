cf1bb69e648ed8b89f5bc235da6e02528a63af10
neznajko <neznajko@yahoo.com>
2025-01-18 20:16:17 +0200

    alt boot

--- lab/script.js
+++ lab/script.js
@@ -23,9 +23,12 @@ function setProp( propName, value ){
 const LASER_ENDPOINTS = [ getProp( ROOT, "--alt-laser-from" ),
                           getProp( ROOT, "--alt-laser-to"   )];
 const DURATION = parseInt( getProp( ROOT, "--alt-duration" ));
+const POINT_BACKGROUND = [ getProp( ROOT, "--alt-body-bgr" ),
+                           "red" ];
 ////////////////////////////////////////////////////////////////
 const altlink = query( ".alt-link" );
 const altlaser = query( ".alt-laser" );
+const altpoint = query( ".alt-point" );
 ////////////////////////////////////////////////////////////////
 const altScanClass = "alt-scan";
 let scan_duration = DURATION;
@@ -47,16 +50,20 @@ altlink.addEventListener( "mouseout", e => {
 ////////////////////////////////////////////////////////////////
 let the_switch = 1;
 let laser_endpoint = LASER_ENDPOINTS[ the_switch ];
+let point_background = POINT_BACKGROUND[ the_switch ];
 function flipflop() {
     the_switch = 1 - the_switch;
     laser_endpoint = LASER_ENDPOINTS[ the_switch ];
+    point_background = POINT_BACKGROUND[ the_switch ];
 }
 ////////////////////////////////////////////////////////////////
 altlaser.addEventListener( "animationend", e => {
     altlaser.style.left = laser_endpoint;
+    altpoint.style.background = point_background;
     setProp( "--alt-laser-from", laser_endpoint );
     flipflop();
     setProp( "--alt-laser-to", laser_endpoint );
+    setProp( "--alt-duration", DURATION );
     scan_duration = DURATION;
 });
 ////////////////////////////////////////////////////////////////
