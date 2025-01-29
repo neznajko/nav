cbaa4c0df20b4c272060cdf3b3dfb00c9985c491
neznajko <neznajko@yahoo.com>
2025-01-29 21:10:16 +0200

    clockwise

--- lab/dropdown/dropdown.js
+++ lab/dropdown/dropdown.js
@@ -1,54 +1,28 @@
 ////////////////////////////////////////////////////////////////
 console.log( "dropdown .." );
 ////////////////////////////////////////////////////////////////
+const link = document.querySelector( ".dropdown-link" );
+const icon = document.querySelector( ".dropdown-anime i" );
 ////////////////////////////////////////////////////////////////
-const anime = document.querySelector( '.dropdown_anime' );
-const link = document.querySelector( '.dropdown_link' );
-const icon = anime.querySelector( 'i' );
-const menu = document.querySelector( '.dropdown_menu' );
-const plus = document.querySelector( '.dropdown_plus' );
-const boom = menu.querySelector( 'span' );
-const spam = plus.querySelector( 'span' );
+let hasDropped = false;
 ////////////////////////////////////////////////////////////////
-// replace here key with state
-let key = false;
-////////////////////////////////////////////////////////////////
-// assing this to .dropdown_anime
-link.addEventListener( 'click', e => {
-    if( key ){
-        icon.classList.add( 'ccw' );
-        icon.classList.remove( 'cw' ); 
-        menu.classList.add( 'close-menu' );
-        menu.classList.remove( 'open-menu' );
-        plus.classList.add( 'close-menu' );
-        plus.classList.remove( 'open-menu' );
+link.addEventListener( "click", e => {
+    if( hasDropped ){
+        icon.classList.remove( "clockwise" );
+        icon.classList.add( "anticlockwise" );
     } else {
-        icon.classList.add( 'cw' );
-        icon.classList.remove( 'ccw' ); 
-        menu.classList.add( 'open-menu' );
-        menu.classList.remove( 'close-menu' );
-        plus.classList.add( 'open-menu' );
-        plus.classList.remove( 'close-menu' );
+        icon.classList.add( "clockwise" );
+        icon.classList.remove( "anticlockwise" );
     }
 });
 ////////////////////////////////////////////////////////////////
-icon.addEventListener( 'animationend', e => {
-    if( key ){
+icon.addEventListener( "animationend", e => {
+    if( hasDropped ){
         icon.style.transform = "rotate( 0deg )";
-        menu.style.height = 0;
-        plus.style.height = 0;
     } else {
         icon.style.transform = "rotate( 90deg )";
-        menu.style.height = "50px";
-        plus.style.height = "50px";
-        boom.style.display = "block";
-        spam.style.display = "block";
     }
-    key = !key;
-});
-icon.addEventListener( 'animationstart', e => {
-    boom.style.display = "none";
-    spam.style.display = "none";
+    hasDropped = !hasDropped;
 });
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
