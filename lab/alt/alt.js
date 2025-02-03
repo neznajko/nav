01cff9640a828057c7ec1e630b4195aeef38d163
neznajko <neznajko@yahoo.com>
2025-02-03 19:48:59 +0200

    indicators

--- lab/alt/alt.js
+++ lab/alt/alt.js
@@ -1,68 +1,56 @@
 ////////////////////////////////////////////////////////////////
 console.log( " a l t " );
 ////////////////////////////////////////////////////////////////
-const LASER_ENDPOINTS = [ getProp( ROOT, "--alt-laser-from" ),
-                          getProp( ROOT, "--alt-laser-to"   )];
-const DURATION = parseInt( getProp( ROOT, "--alt-duration" ));
-const POINT_BACKGROUND = [ getProp( ROOT, "--alt-body-bgr" ),
-                           "red" ];
+const indicators = queryall( ".alt-indicator" );
+const links = queryall( ".alt-indicator + a" );
 ////////////////////////////////////////////////////////////////
-const altlink = query( ".alt-link" );
-const altlaser = query( ".alt-laser" );
-const altpoint = query( ".alt-point" );
+const n = links.length;
 ////////////////////////////////////////////////////////////////
-const altScanClass = "alt-scan";
-let scan_duration = DURATION;
+const BACKWARD = false;
+const FORWARD = true;
 ////////////////////////////////////////////////////////////////
-const start_scan = e => {
-    // there is some strange jumping without thus line
-    getProp( altlaser, "left" );
-    altlaser.classList.add( altScanClass );
+const animation_direction = new Array( n ).fill( FORWARD );
+////////////////////////////////////////////////////////////////
+function addClass( domElement, className ){
+    domElement.classList.add( className );
 }
 ////////////////////////////////////////////////////////////////
-const stop_scan = e => {
-    const left = getProp( altlaser, "left" );
-    setProp( "--alt-laser-from", left );
-    altlaser.style.left = left;
-    scan_duration *= 0.7;
-    setProp( "--alt-duration", scan_duration + "s" );
-    altlaser.classList.remove( altScanClass );
+function removeClass( domElement, className ){
+    domElement.classList.remove( className );
 }
 ////////////////////////////////////////////////////////////////
-altlink.addEventListener( "mouseover", e => {
-    start_scan( e );
-});
-////////////////////////////////////////////////////////////////
-altlink.addEventListener( "touchstart", e => {
-    start_scan( e );
-});
-////////////////////////////////////////////////////////////////
-altlink.addEventListener( "touchend", e => {
-    stop_scan( e );
-});
-////////////////////////////////////////////////////////////////
-altlink.addEventListener( "mouseout", e => {
-    stop_scan( e );
-});
-////////////////////////////////////////////////////////////////
-let the_switch = 1;
-let laser_endpoint = LASER_ENDPOINTS[ the_switch ];
-let point_background = POINT_BACKGROUND[ the_switch ];
-function flipflop() {
-    the_switch = 1 - the_switch;
-    laser_endpoint = LASER_ENDPOINTS[ the_switch ];
-    point_background = POINT_BACKGROUND[ the_switch ];
+for( let j = 0; j < n; ++j ){
+    const link = links[ j ];
+    const indicator = indicators[ j ];
+    // add click event listeners to links
+    link.addEventListener( "click", e => {
+        if( animation_direction[ j ] == FORWARD ){
+            removeClass( indicator, "backward_indicator_animation" );
+            addClass(    indicator,  "forward_indicator_animation" );
+        } else {
+            removeClass( indicator,  "forward_indicator_animation" );
+            addClass(    indicator, "backward_indicator_animation" );
+        }        
+    });
+    // add animationend event listeners to indicators
+    indicator.addEventListener( "animationend", e => {
+        if( animation_direction[ j ] == FORWARD ){
+            indicator.style.opacity = "1";
+        } else {
+            indicator.style.opacity = "0";
+        }
+        animation_direction[ j ] = !animation_direction[ j ];
+    });
 }
 ////////////////////////////////////////////////////////////////
-altlaser.addEventListener( "animationend", e => {
-    altlaser.style.left = laser_endpoint;
-    altpoint.style.background = point_background;
-    setProp( "--alt-laser-from", laser_endpoint );
-    flipflop();
-    setProp( "--alt-laser-to", laser_endpoint );
-    setProp( "--alt-duration", DURATION );
-    scan_duration = DURATION;
-});
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
