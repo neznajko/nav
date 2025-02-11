7c3fdc13fbf6aafe86d7feab91f1170c4a3de4fd
neznajko <neznajko@yahoo.com>
2025-02-11 22:27:47 +0200

    links

--- lab/dub/dub.js
+++ lab/dub/dub.js
@@ -1,3 +1,43 @@
 ////////////////////////////////////////////////////////////////
 console.log( "dubing .." );
 ////////////////////////////////////////////////////////////////
+class Dom {
+    constructor( selector ){
+        this.elem = document.querySelector( selector );
+    }
+}
+////////////////////////////////////////////////////////////////
+class Light extends Dom {
+    constructor( selector ){
+        super( selector );
+    }
+    hide() {
+        this.elem.setAttribute( "visibility", "hidden" );
+    }
+    show() {
+        this.elem.setAttribute( "visibility", "visible" );
+    }
+}
+////////////////////////////////////////////////////////////////
+class Link extends Dom {
+    constructor( name ){
+        super( ".dub-" + name );
+        this.light = new Light( "#" + name + "-light" );
+        this.light.hide();
+        this.subscribe();
+    }
+    subscribe() {
+        this.elem.addEventListener( "mouseenter", e => {
+            this.light.show();
+        });
+        this.elem.addEventListener( "mouseleave", e => {
+            this.light.hide();
+        });
+    }
+}
+////////////////////////////////////////////////////////////////
+const deckLink = new Link( "deck" );
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
