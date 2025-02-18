0e3ee8bfa6965d536a0c48ff380c68418f96313f
neznajko <neznajko@yahoo.com>
2025-02-18 15:27:02 +0200

    colors

--- folder.js
+++ folder.js
@@ -13,16 +13,16 @@ const _this_base = folder.querySelector( ".this-base"  );
 const out_base   = folder.querySelector( ".out-base"   );
 const brand = folder.querySelector( ".folder-brand" );
 let zindex = 3;
-function addEventListener( link, base ){
+function _addEventListener( link, base ){
     link.addEventListener( "click", e => {
         link.style.zIndex = zindex;
         base.style.zIndex = zindex;
         brand.style.zIndex = ++zindex;
     });
 }
-addEventListener( check, check_base );
-addEventListener( _this, _this_base );
-addEventListener( out, out_base );
+_addEventListener( check, check_base );
+_addEventListener( _this, _this_base );
+_addEventListener( out, out_base );
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
